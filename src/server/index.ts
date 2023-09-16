b115a4c9a1a7f2cb70aeb89bc2b7c04ddb8bf25b
Metee <viixxivvimm@gmail.com>
2023-09-16 16:10:11 +0700

    fix: export ServerEventContext

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,4 @@
 export * from "@censor1337/cfx-core/server";
 export * from "../shared";
-export { Event, ServerEvent } from "./Event";
+export { Event, ServerEvent, ServerEventContext } from "./Event";
 export { Player } from "./Player";
