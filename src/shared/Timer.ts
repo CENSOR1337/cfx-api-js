47c28e43cd20d2eed0a8d065b55f3f6631ce2610
Metee <viixxivvimm@gmail.com>
2023-09-16 16:05:36 +0700

    refactor: chaneg `Tick` to `Timer`

---
+++ src/shared/Timer.ts
@@ -0,0 +1,30 @@
+import { Citizen } from "./Citizen";
+
+interface CitizenTimer {
+	ref(): CitizenTimer;
+	unref(): CitizenTimer;
+	hasRef(): boolean;
+	refresh(): CitizenTimer;
+	[Symbol.toPrimitive](): number;
+}
+
+const onTick = function (callback: Function) {
+	return Citizen.setTick(callback);
+};
+
+const clearTick = function (tick: number) {
+	Citizen.clearTick(tick);
+};
+
+function setTimeout<T extends any[]>(callback: (...args: T) => void, ms?: number, ...args: T): CitizenTimer {
+	return Citizen.setTimeout(callback, ms, ...args);
+}
+
+function clearTimeout(timeout: CitizenTimer): void {
+	//@ts-ignore
+	return Citizen.clearTimeout(timeout);
+}
+
+export { CitizenTimer };
+export { onTick, onTick as everyTick, clearTick };
+export { setTimeout, clearTimeout };
