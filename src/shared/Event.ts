8db42c02d2b5574aa459212327e90ff0469787bf
Metee <viixxivvimm@gmail.com>
2023-09-27 21:57:45 +0700

    fix: EventEmitter
     * Event component only bind to CFX event only once.

--- src/shared/Event.ts
+++ src/shared/Event.ts
@@ -6,6 +6,77 @@ export interface EventData {
 	listener: listenerType;
 }
 
+export interface IEventEmitter {
+	eventname: string;
+	listenerId: number;
+}
+
+const events = new Map<string, EventEmitter>();
+
+class EventEmitter {
+	public readonly eventname: string;
+	private readonly listeners = new Map<number, Function>();
+	private listenerId = 0;
+	private listener: listenerType;
+	private netSafe: boolean;
+
+	constructor(eventname: string, netSafe = false) {
+		this.eventname = eventname;
+		this.netSafe = netSafe;
+		this.listener = (...args: any[]) => {
+			this.onEventInvoke(...args);
+		};
+
+		Citizen.addEventListener(this.eventname, this.listener, this.netSafe);
+		events.set(this.eventname, this);
+	}
+
+	private onEventInvoke(...args: any[]): void {
+		this.listeners.forEach((listener: Function, id: number) => {
+			listener(...args);
+		});
+	}
+
+	public addListener(listener: Function, netSafe = false): IEventEmitter {
+		if (netSafe && !this.netSafe) {
+			Citizen.removeEventListener(this.eventname, this.listener);
+			Citizen.addEventListener(this.eventname, this.listener, netSafe);
+			this.netSafe = true;
+		}
+		this.listenerId++;
+		this.listeners.set(this.listenerId, listener);
+		return {
+			eventname: this.eventname,
+			listenerId: this.listenerId,
+		};
+	}
+
+	public removeListener(listenerId: number): void {
+		this.listeners.delete(listenerId);
+
+        const eventInstance = events.get(this.eventname);
+        if (!eventInstance) return;
+        if (this.listeners.size > 0) return;
+        events.delete(this.eventname);
+	}
+}
+
+function addEvent(eventname: string, listener: Function, netSafe = false): IEventEmitter {
+	let eventInstance = events.get(eventname);
+	if (!eventInstance) {
+		eventInstance = new EventEmitter(eventname);
+	}
+
+	return eventInstance.addListener(listener, netSafe);
+}
+
+function removeEvent(eventData: IEventEmitter) {
+	const eventInstance = events.get(eventData.eventname);
+	if (!eventInstance) return;
+
+    eventInstance.removeListener(eventData.listenerId);
+}
+
 export class EventBase {
 	public static getClassFromArguments(...args: any[]): any[] {
 		const newArgs: any[] = [];
@@ -38,6 +109,7 @@ export class EventBase {
 export class Event extends EventBase {
 	private eventName: string;
 	private listener: listenerType;
+	private event: IEventEmitter;
 	private netSafe: boolean;
 	private once: boolean;
 
@@ -50,11 +122,11 @@ export class Event extends EventBase {
 			listener(...args);
 			if (this.once) this.destroy();
 		};
-		Citizen.addEventListener(this.eventName, this.listener, this.netSafe);
+		this.event = addEvent(this.eventName, this.listener, this.netSafe);
 	}
 
 	destroy(): void {
-		Citizen.removeEventListener(this.eventName, this.listener);
+		removeEvent(this.event);
 	}
 
 	public static on(eventName: string, listener: listenerType): Event {
