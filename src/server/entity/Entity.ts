e0e10aa4424ea2f84985674e1a2397a30531fde3
Metee <viixxivvimm@gmail.com>
2023-10-25 04:22:02 +0700

    feat: add general methods

--- src/server/entity/Entity.ts
+++ src/server/entity/Entity.ts
@@ -1,12 +1,21 @@
 import { WorldObject } from "../../shared/entity/WorldObject";
 import * as cfx from "@censor1337/cfx-core/server";
 import { Vector3 } from "@censor1337/cfx-core/server";
+import { Player } from ".";
 
 export class Entity extends WorldObject {
 	public readonly id: number;
 	constructor(id: number) {
 		super();
 		this.id = id;
+	}
+
+	public get valid(): boolean {
+		return cfx.doesEntityExist(this.handle);
+	}
+
+	public get exists(): boolean {
+		return this.valid;
 	}
 
 	public get handle(): number {
@@ -19,5 +28,37 @@ export class Entity extends WorldObject {
 
 	public get dimension(): number {
 		return cfx.getEntityRoutingBucket(this.handle);
+	}
+
+	public get netOwner(): Player | undefined {
+		const owner = cfx.networkGetEntityOwner(this.handle);
+		if (owner <= 0) return undefined;
+		return Player.fromSource(owner);
+	}
+
+	public get firstOwner(): Player | undefined {
+		const firstOwner = cfx.networkGetFirstEntityOwner(this.handle);
+		if (firstOwner <= 0) return undefined;
+		return Player.fromSource(firstOwner);
+	}
+
+	public set pos(pos: Vector3) {
+		cfx.setEntityCoords(this.handle, pos.x, pos.y, pos.z, false, false, false, false);
+	}
+
+	public get rot(): Vector3 {
+		return cfx.getEntityRotation(this.handle);
+	}
+
+	public set rot(rot: Vector3) {
+		cfx.setEntityRotation(this.handle, rot.x, rot.y, rot.z, 0, false);
+	}
+
+	public set dimension(dimension: number) {
+		cfx.setEntityRoutingBucket(this.handle, dimension);
+	}
+
+	public get model(): number {
+		return cfx.getEntityModel(this.handle);
 	}
 }
